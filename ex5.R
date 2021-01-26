b68b56c678885706f4a68fc071ea6892b92ee0cd
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-01-26 22:36:31 +0200

    prima varianta la ex5

---
+++ ex5.R
@@ -0,0 +1,162 @@
+ 
+# -hackish function
+# -asa pot ridica o functie la puterea y si ca retval sa am tot o functie
+# -f useful pt momente
+# O mica problema cu approach-ul asta e ca se pare ca R trimite ca argument
+# functiile ca referinte, nu cu shallow copy.
+# Implicatia e ca daca schimbi f in afara lui pow, o sa fie schimbat si cand e
+# aplicat de pow.
+# Daca faci ceva de genu myFunc <- pow(myFunc,2), obtii o recursie infinita.
+# Solutia este sa faci raisedFunc <- pow(myFunc, 2)
+pow <- function(f, y)
+{
+  ret <- function(x)
+  {
+    f(x) ^ y
+  }
+}
+
+# returneaza valoarea mediei sau o eroare
+media <- function(fdens)
+{
+  # evil R magic
+  fdens <- Vectorize(fdens)
+  
+  # -valoarea de sub integrala trebuie trimisa
+  # ca o functie, cel mai lejer de scris e asa
+  subIntegrala <- function(x)
+  {
+    x * fdens(x)
+  }
+  
+  # more evil R magic
+  subIntegrala <- Vectorize(subIntegrala)
+  
+
+  retval <- 0  
+  tryCatch(
+    retval <- integrate(subIntegrala, -Inf, Inf)$value,
+    error=function(err){
+      stop("Media este divergenta")
+    }
+  )
+}
+
+dispersia <- function(fdens)
+{
+  # -deoarece momentan lucrez doar cu functii de densitate, voi folosi
+  # formula clasica cu integrala
+  # -cand e gata clasa de variabile, putem incerca si formula aia mai rapida
+  
+  m <- 0
+  tryCatch(m <- media(fdens), warning=function(wr)
+    {
+      stop("Calcularea dispersiei a esuat")
+      m <<- -Inf # dublu << inseamna global scope pe m
+    }) 
+  if(m == -Inf)
+    return(-1)
+  
+  xCoef <- function(x)
+  {
+    x - m
+  }
+    
+  coefRaised <- pow(xCoef, 2)
+  coefRaised <- Vectorize(coefRaised)
+  
+  subIntegrala <- function(x)
+  {
+    coefRaised(x) * fdens(x)
+  }
+  subIntegrala <- Vectorize(subIntegrala)
+  
+  tryCatch(retval <- integrate(subIntegrala, -Inf, Inf)$value,
+           error= function(err)
+           {
+             warning("Dispersia este divergenta")
+           })
+}
+
+
+# -referitor la momente, nu sunt sigur daca o strategie mai buna
+# ar fi sa scriem o singura functie moment, in care sa se indice cu un
+# parametru daca e centrat sau initial, ca sa mai economisim cod
+# -dezavantajul e evident ca functia generala de moment ar arata mai urat
+moment_centrat <- function(fdens, ordin)
+{
+  # cazurile triviale
+  if(ordin == 0)
+      return(1)
+  else if(ordin == 1)
+      return(0)
+  else if(ordin == 2)
+      return(dispersia(fdens))
+  
+  m <- 0
+  tryCatch(m <- media(fdens), warning=function(wr)
+  {
+    stop("Calcularea momentului centrat de ordin " + ordin + " a esuat")
+    m <<- -Inf # dublu << inseamna global scope pe m
+  }) 
+  if(m == -Inf)
+    return(-1)
+  
+  xCoef <- function(x)
+  {
+    x - m
+  }
+  
+  coefRaised <- pow(xCoef, ordin)
+  coefRaised <- Vectorize(coefRaised)
+  
+  subIntegrala <- function(x)
+  {
+    coefRaised(x) * fdens(x)
+  }
+  subIntegrala <- Vectorize(subIntegrala)
+  
+  tryCatch(retval <- integrate(subIntegrala, -Inf, Inf)$value,
+           error= function(err)
+           {
+             warning("Momentul centrat de ordin " + ordin + " nu exista")
+           })
+}
+
+moment_initial <- function(fdens, ordin)
+{
+  # cazurile triviale
+  if(ordin == 0)
+    return(1)
+  else if(ordin == 1)
+    return(media(fdens))
+  
+  m <- 0
+  tryCatch(m <- media(fdens), warning=function(wr)
+  {
+    stop("Calcularea momentului initial de ordin " + ordin + " a esuat")
+    m <<- -Inf # dublu << inseamna global scope pe m
+  }) 
+  if(m == -Inf)
+    return(-1)
+  
+  xCoef <- function(x)
+  {
+    x
+  }
+  
+  coefRaised <- pow(xCoef, ordin)
+  coefRaised <- Vectorize(coefRaised)
+  
+  subIntegrala <- function(x)
+  {
+    coefRaised(x) * fdens(x)
+  }
+  subIntegrala <- Vectorize(subIntegrala)
+  
+  tryCatch(retval <- integrate(subIntegrala, -Inf, Inf)$value,
+           error= function(err)
+           {
+             warning("Momentul initial de ordin " + ordin + " nu exista")
+           })
+}

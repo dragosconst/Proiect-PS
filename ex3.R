fbc02e8bbdcf969b53470505f29be028d41d8373
Edi112263 <vintilaeduardionut@yahoo.com>
2021-01-27 15:52:43 +0200

    modificare ex. 3

--- ex3.R
+++ ex3.R
@@ -1,37 +1,49 @@
 # definire clasa 
-setClass("contRV", slots=list(densitate="function", bidimen="logical"))
+setClass("contRV", slots=list(densitate="function", bidimen="logical", domeniu="numeric"))
 
 # un fel de constructor
-contRV <- function(densitate, bidimen)
+contRV <- function(densitate, bidimen, domeniu = c(-Inf, Inf))
 {
     
     # aici de verificat daca functia data este densitate de probabilitate
-    obj <- new("contRV", densitate = densitate, bidimen = bidimen)
+    obj <- new("contRV", densitate = densitate, bidimen = bidimen, domeniu = domeniu)
     
     return (obj)
 }
 
-# declararea functiei de verificare v.a bidimensionala
-setGeneric("verifbidimen", function(object) standardGeneric("verifbidimen"))
 
-# definirea functiei pentru clasa "contRV"
-setMethod("verifbidimen", "contRV", 
-          function (object) {
-              if (object@bidimen == TRUE)
-                  print("Este bidimensionala!")
-              else
-                  print ("Nu este bidimensionala!")
+# asta e un inceput pentru ex. 7
+if (!isGeneric("P"))
+    setGeneric("P", function(object, x) standardGeneric("P"))
+    
+setMethod("P", "contRV", 
+          function (object, x) {
+              integrate(f = object@densitate, lower = object@domeniu[1], upper = x)
           })
 
 # supraincarcare functie de afisare
 setMethod("show", "contRV",
           function (object) {
-              cat("Denistatea de probabilitate: ")
+              cat("Densitatea de probabilitate: ")
               print(body(fun = object@densitate))
               cat("Este v.a bidimensionala: ", object@bidimen, "\n")
           })
 
+# by Florin
+func <- function(x)
+{
+    if (x < -1)
+        0
+    else if (x < 0)
+        1 + x
+    else if (x < 1)
+        1 - x
+    else
+        0
+}
+
 # exemple
-X <- contRV(densitate = function(x, y) 2*x + 3*y, bidimen = TRUE)
-verifbidimen(X)
+X <- contRV(densitate = Vectorize(func), bidimen = FALSE)
 X
+integrate(Vectorize(func), -4, Inf, rel.tol = .Machine$double.eps^0.5)
+

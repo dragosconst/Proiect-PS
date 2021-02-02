9b9934483ea50dcfff8e2f80cfa6a4dad1d6652b
JustMike13 <62668963+JustMike13@users.noreply.github.com>
2021-02-02 18:01:17 +0200

    fix la ex11

--- ex11.R
+++ ex11.R
@@ -1,6 +1,6 @@
 ex11 <- function(X){
   dens_marginala_x <- integrala(X,2)
   dens_marginala_y <- integrala(X,1)
-  dens_condit_x_la_y = dens_comuna / dens_marginala_y
-  dens_condit_y_la_x = dens_comuna / dens_marginala_x
+  dens_condit_x_la_y = X@densitate / dens_marginala_y
+  dens_condit_y_la_x = X@densitate / dens_marginala_x
 }

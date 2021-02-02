389aaa8f1b035a68f5458abdb561a477bb1f25a9
JustMike13 <62668963+JustMike13@users.noreply.github.com>
2021-02-02 18:41:36 +0200

    Update ex11.R

--- ex11.R
+++ ex11.R
@@ -1,6 +1,14 @@
-ex11 <- function(X){
-  dens_marginala_x <- integrala(X,2)
-  dens_marginala_y <- integrala(X,1)
-  dens_condit_x_la_y = X@densitate / dens_marginala_y
-  dens_condit_y_la_x = X@densitate / dens_marginala_x
+
+dens_condit_x_la_y <- function(z)
+{
+  dens_marginala_y <- integrala(z,1)
+  f <- function(x,y) {if (dens_marginala_y(y) != 0) {X@densitate(x,y) / dens_marginala_y(y)} else stop("Densitatea marginala este 0")}
+  return (Vectorize(f))
+}
+
+dens_condit_y_la_x <- function(z)
+{
+  dens_marginala_x <- integrala(z,2)
+  f <- function(x,y) {if (dens_marginala_y(y) != 0) {X@densitate(x,y) / dens_marginala_x(x)} else stop("Densitatea marginala este 0")}
+  return (Vectorize(f))
 }
